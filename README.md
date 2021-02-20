f45464a63abfa6c3744e883b6c8672406a3901e6
crystal-g-b <crystal.barnett1@gmail.com>
2021-02-20 18:09:42 -0500

    adding variables to the JS file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Password-Generator
+
+For this Assignment, we were required to create a password generator with prompts.
+
+1. I added variables for the prompts to the JavaScript file. 
--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numChoice = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var specialChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "~", "}", "{", "|", "_", "^" ];
+
+
+
+
+
+//when I click on generate password button then I am presented with a series of prompts for password criteria
+generateBtn = window.prompt("length of password")
+
 
 // Write password to the #password input
 function writePassword() {
